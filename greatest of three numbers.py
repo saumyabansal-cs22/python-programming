37c4a11aff396613058fe16956c21855c293ba30
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-02-11 01:30:52 +0530

    Add files via upload

---
+++ greatest of three numbers.py
@@ -0,0 +1,13 @@
+a=int(input("enter first number;"))
+b=int(input("enter second number;"))
+c=int(input("enter third number;"))
+if (a>b):
+    if (a>c):
+        print("a is greatest;")
+    else:
+        print("c is greatest;")
+else:
+    if (b>c):
+        print(" b is greatest;")
+    else:
+        print(" c is greatest;")
