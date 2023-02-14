f0f514668e2126b6901395767163367aeb1c1c3a
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-02-14 22:35:08 +0530

    Add files via upload

---
+++ no of tiles.py
@@ -0,0 +1,7 @@
+l=eval(input("enter the length of room="))
+b=eval(input("enter the breadth of room="))
+x=eval(input("enter length of tile="))
+y=eval(input("enter breadth of tile="))
+ar=l*b
+at=x*y
+print(f'no. of tiles={ar//at}')
