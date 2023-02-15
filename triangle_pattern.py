006e8f15ac033750066c40a13e78328917c7c957
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-02-15 22:35:26 +0530

    Add files via upload

---
+++ triangle_pattern.py
@@ -0,0 +1,10 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end='')
+    print( )
+for k in range(5,1,-1):
+    for m in range(k,1,-1):
+        print("*",end='')
+    print( )
+
+    
