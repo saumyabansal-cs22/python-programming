b129699dc3f3eaa2d4bb1448ac50cfbcbfd590a5
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-03-08 15:24:36 +0530

    Add files via upload

---
+++ hollow triangle using single loop.py
@@ -0,0 +1,14 @@
+"""
+*
+*  *
+*    *
+*      *
+* * * * *
+"""
+n=5
+for i in range(1,n+1):
+    if i==1 or i==n:
+        print("* "*i)
+    else:
+        print("*","  "*(i-1),"*",sep='')
+print()
