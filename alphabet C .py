9654adf1ab7609ade31490363eecd6d7d5de3788
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-03-26 21:50:35 +0530

    Add files via upload

---
+++ alphabet C .py
@@ -0,0 +1,11 @@
+n=int(input())
+w=int(input())
+for i in range(n):
+    for j in range(w):
+        if i==0 or i==n-1:
+            print('*',end=' ')
+        elif j==0:
+            print('*',end=' ')
+        else: 
+            print(' ',end=' ')
+    print()
