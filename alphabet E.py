46d8b695ac7bb743b96c5b664148158ab4041600
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-03-26 21:50:58 +0530

    Add files via upload

---
+++ alphabet E.py
@@ -0,0 +1,12 @@
+n=int(input())
+w=int(input())
+m=n//2
+for i in range(n):
+    for j in range(w):
+        if i==0 or i==m or i==n-1:
+            print('*',end=' ')
+        elif j==0 or j==w:
+            print('*',end=' ')
+        else: 
+            print(' ',end=' ')
+    print()
