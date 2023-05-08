9c5e23cecd7cd72d293c3786a5d850ed811b7c45
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-05-09 03:46:59 +0530

    Add files via upload

---
+++ docstrings.py
@@ -0,0 +1,6 @@
+def square(n):
+    '''Take the number and returns the square'''
+    # string literals written just after function name,modules, class etc
+    print(n**2)
+square(5)
+print(square.__doc__)
