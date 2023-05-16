9ac277a781c27bea8e98eb9da2220409e9cb46b5
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-05-16 22:00:54 +0530

    Add files via upload

---
+++ exception handling.py
@@ -0,0 +1,10 @@
+a=input("enter the number: ")
+print(f"multiplication of {a} is :")
+try:
+    for i in range(1,11):
+        print(f"{int(a)} x {i} = {int(a)*i}")
+except:
+    print("Invalid option")
+print("Some lines of code")
+print("Program ends here")
+
